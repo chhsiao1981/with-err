c30fa20ff125aa84b38fee47caea6bc0e196f8ac
Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>
2026-09-13 21:06:53 -0400

    feat: add funcs for iterables. (#33)
    1. get_errs.
    2. get_results.
    3. get_results_or_none.
    4. is_any_err.
    5. is_all_err.
    6. get_first_err.
    
    Signed-off-by: Chuan-Heng Hsiao <2970164+chhsiao1981@users.noreply.github.com>

--- src/with_err/__init__.py
+++ src/with_err/__init__.py
@@ -1,5 +1,13 @@
 from .raise_err import raise_err
-from .utils import get_err_strs
+from .utils import (
+    get_err_strs,
+    get_errs,
+    get_first_err,
+    get_results,
+    get_results_or_none,
+    is_all_err,
+    is_any_err,
+)
 from .with_err import (
     Result,
     with_async_err,
@@ -15,6 +23,12 @@ from .with_err import (
 __all__ = [
     'Result',
     'get_err_strs',
+    'get_errs',
+    'get_first_err',
+    'get_results',
+    'get_results_or_none',
+    'is_all_err',
+    'is_any_err',
     'raise_err',
     'with_async_err',
     'with_async_exc',
